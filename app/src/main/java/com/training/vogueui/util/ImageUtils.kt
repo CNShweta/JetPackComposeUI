4fa5603dd8488747ea1b0ce1936c48beddbb75c2
Charu Agrawal <charu_agrawal@condenast.com>
2022-11-03 14:30:06 +0530

    remove code

--- app/src/main/java/com/training/vogueui/util/ImageUtils.kt
+++ app/src/main/java/com/training/vogueui/util/ImageUtils.kt
@@ -1,11 +1,11 @@
 package com.training.vogueui.util
 
-import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
@@ -17,17 +17,16 @@ import com.bumptech.glide.request.transition.Transition
  * @param imageUrl url of the image to be loaded
  * @return bitmapState value from given imageUrl
  */
-@SuppressLint("UnrememberedMutableState")
 @Composable
 fun loadImageUsingGlide(imageUrl: String): MutableState<Bitmap?> {
 
-    val bitmapState: MutableState<Bitmap?> = mutableStateOf(null)
+    val bitmapState: MutableState<Bitmap?> = remember { mutableStateOf(null) }
 
     Glide.with(LocalContext.current)
         .asBitmap()
         .load(imageUrl)
         .into(object : CustomTarget<Bitmap>() {
-            override fun onLoadCleared(placeholder: Drawable?) { }
+            override fun onLoadCleared(placeholder: Drawable?) {}
             override fun onResourceReady(
                 resource: Bitmap,
                 transition: Transition<in Bitmap>?
