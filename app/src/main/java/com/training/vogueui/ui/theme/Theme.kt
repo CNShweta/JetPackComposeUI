1d8073ac64f8cf166466bf553bec71fd9ca2f1a1
shweta kumari <Shweta_kumari@condenast.com>
2022-11-04 12:42:50 +0530

    updated version number

--- app/src/main/java/com/training/vogueui/ui/theme/Theme.kt
+++ app/src/main/java/com/training/vogueui/ui/theme/Theme.kt
@@ -3,10 +3,13 @@ package com.training.vogueui.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun VogueUITheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setStatusBarColor(Color.Black)
     MaterialTheme(
         typography = Typography,
         shapes = Shapes,
