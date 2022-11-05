258e8df17178ac97b90366d0fba4fdb390cbdc8f
shweta kumari <Shweta_kumari@condenast.com>
2022-11-05 12:51:12 +0530

    theme

--- app/src/main/java/com/training/vogueui/ui/theme/Theme.kt
+++ app/src/main/java/com/training/vogueui/ui/theme/Theme.kt
@@ -2,20 +2,58 @@ package com.training.vogueui.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun VogueUITheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val systemUiController = rememberSystemUiController()
-    systemUiController.setStatusBarColor(Color.Black)
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
     MaterialTheme(
+        colors = colors,
         typography = Typography,
         shapes = Shapes,
         content = content
     )
+    val systemUiController = rememberSystemUiController()
+    DisposableEffect(systemUiController, darkTheme) {
+        // Update all of the system bar colors to be transparent, and use
+        // dark icons if we're in light theme
+        var color = if(darkTheme){
+            Color.Black
+        }else{
+            Color.Black
+        }
+        systemUiController.setSystemBarsColor(
+            color = color
+        )
+        systemUiController.setStatusBarColor(color, darkIcons = darkTheme)
+
+        onDispose {}
+    }
+
 }
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+    background = Color.Black
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+    background = Color.White
+)
 
 
 
