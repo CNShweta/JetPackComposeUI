f7f1d8e842c11591a36efd3c3f8044eae7bbcc18
shweta kumari <Shweta_kumari@condenast.com>
2022-11-10 12:21:48 +0530

    added button and text

--- app/src/main/java/com/training/vogueui/screen/FeaturedShows.kt
+++ app/src/main/java/com/training/vogueui/screen/FeaturedShows.kt
@@ -1,10 +1,13 @@
 package com.training.vogueui.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -13,9 +16,17 @@ import com.training.vogueui.ui.theme.RunwayWhite
 
 @Composable
 fun FeaturedShows(navController: NavController, modifier: Modifier){
+    var textFieldState by remember {
+        mutableStateOf("Loading........")
+    }
     Box(modifier = modifier
         .fillMaxSize()
         .background(RunwayWhite), contentAlignment = Alignment.Center){
-        Text(text = "Featured Shows")
+        Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+           Button(onClick = { textFieldState = "Loaded" }) {
+               Text(text = "Click")
+           }
+            Text(text = textFieldState)
+        }
     }
 }
